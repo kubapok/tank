d0c682e1c40f57314ba0e590f2e6b7a2db7175ff
kubapok <jakubpokrywka@gmail.com>
2015-10-20 21:56:27 +0200

    more graphics objects

--- Main.py
+++ Main.py
@@ -7,11 +7,14 @@ pygame.init()
 FPS = 15 # frames per second setting
 fpsClock = pygame.time.Clock()
 # set up the window
-DISPLAYSURF = pygame.display.set_mode((900, 700), 0, 32)
+DISPLAYSURF = pygame.display.set_mode((900, 600), 0, 32)
 pygame.display.set_caption('tank game')
 WHITE = (255, 255, 255)
 
 tank = Tank(100,100)
+tracks = pygame.image.load('Images/tracks.png')
+lake = pygame.image.load('Images/lake.png')
+tree = pygame.image.load('Images/tree.png')
 
 while True: # the main game loop
     DISPLAYSURF.fill(WHITE)
@@ -21,15 +24,19 @@ while True: # the main game loop
         (tank.x == 300 and tank.y == 300) or\
         (tank.x == 100 and tank.y == 100):
         tank.turnRight()
+
     tank.move()
 
-    print(tank.direction)
+    #print(tank.direction)
 
+    DISPLAYSURF.blit(tracks, (0, 0))
+    DISPLAYSURF.blit(lake, (0, 0))
+    DISPLAYSURF.blit(tree, (10, 10))
     DISPLAYSURF.blit(tank.lower, (tank.x, tank.y))
     DISPLAYSURF.blit(tank.upper, (tank.x, tank.y))
 
-
     for event in pygame.event.get():
+
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
