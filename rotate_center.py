7449e4a9f6e9f9f29773982907ac6f030822849b
kubapok <jakubpokrywka@gmail.com>
2016-01-26 15:46:05 +0100

    removed *.pyc and other garbage

--- rotate_center.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import pygame
-
-def rotate_center(image, angle):
-    center_of_image = image.get_rect().center
-    rotated_image = pygame.transform.rotate(image, angle)
-    rotated_image.get_rect().center = center_of_image
-    return rotated_image
